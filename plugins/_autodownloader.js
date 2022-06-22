55ab722134b5321d166692616c703eb4481a8136
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-06-22 12:47:13 +0700

    Add files via upload

---
+++ plugins/_autodownloader.js
@@ -0,0 +1,22 @@
+let { tiktokdl } = require("@bochilteam/scraper")
+let handler = m => m
+
+handler.before = async function (m, { conn, text }) {
+  if (m.text.includes("tiktok.com")) {
+  let url = await findUrl(m.text)
+  url.forEach(async(link, i) => {
+    setTimeout(async() => {
+  let buff = await tiktokdl(link)
+  await conn.sendMessage(m.chat, {video: { url: buff.video.no_watermark}}, {quoted: m})
+   
+    }, 1000 * i)
+   })
+  }
+}
+
+module.exports = handler
+
+async function findUrl(message) {
+  var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+  return message.match(urlRegex)
+}
