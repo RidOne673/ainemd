f52266303af53e5ac7ef0bc60c2330bac298601d
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-06-28 13:43:06 +0700

    Add files via upload

--- plugins/play.js
+++ plugins/play.js
@@ -1,54 +1,38 @@
 let limit = 30
 let yts = require('yt-search')
-let fetch = require('node-fetch')
-const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => {
-  if (!text) throw `What song are you want find?\n*Example:* ${usedPrefix}play alan walker faded`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Not Found'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry another server...'}`)
-    }
-  }
-  if (yt === false) throw 'All servers can\'t 😕'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${await shortlink(dl_link)}
-*Server y2mate:* ${usedServer}
-
-_*Please wait while processing..*_
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+if (!text) throw `Example : .${command} story wa anime`
+let search = await yts(text)
+let anu = search.videos[Math.floor(Math.random() * 1)]
+let buttons = [
+{buttonId: `.ytmp3 ${anu.url}`, buttonText: {displayText: '♫ Audio'}, type: 1},
+{buttonId: `.ytmp4 ${anu.url}`, buttonText: {displayText: '► Video'}, type: 1}
+]
+let buttonMessage = {
+image: { url: anu.thumbnail },
+caption: `
+⭔ Title : ${anu.title}
+⭔ Ext : Search
+⭔ ID : ${anu.videoId}
+⭔ Duration : ${anu.timestamp}
+⭔ Viewers : ${anu.views}
+⭔ Upload At : ${anu.ago}
+⭔ Author : ${anu.author.name}
+⭔ Channel : ${anu.author.url}
+⭔ Description : ${anu.description}
+⭔ Url : ${anu.url}`,
+footer: conn.user.name,
+buttons: buttons,
+headerType: 4
 }
-handler.help = ['play', 'play2'].map(v => v + ' <search>')
+let msg = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+setTimeout (async() => {
+conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: msg.key.id, participant: msg.key.remoteJid } })
+}, 10000)
+}
+handler.help = ['play'].map(v => v + ' <search>')
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
+handler.command = /^play?$/i
 handler.premium = false
 handler.group = false
 
