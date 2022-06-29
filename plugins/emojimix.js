05c602978437ad46409a5a31a17853aa0562d6e5
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-06-29 23:27:05 +0700

    Add files via upload

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -1,81 +1,26 @@
-/*const fetch = require('node-fetch')
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
-let handler = async (m, { conn, text, args, usedPrefix }) => {
-	let er = `Masukan format!!
-
-Example:
-${usedPrefix}emojimix 🤨😠`
-
-let ers = `*Emoticon is not supported.*`
-if (!text) throw er
-try {
-	let res = await fetchJson('https://api.neoxr.eu.org/api/emoji?q=' + encodeURIComponent(text) + '&apikey=yourkey')
-    let stiker = await sticker(false, res.data.url, global.packname, global.author)
-              await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-                quoted: m
-              })
-         } catch (e) {
-      throw ers
-    }
-}
-
-handler.help = ['emojimix']
-handler.tags = ['sticker']
-handler.command = /^(emojimix)$/i
-handler.limit = true 
-module.exports = handler
-
-
-const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-    fetch(url, options)
-        .then(response => response.json())
-        .then(json => {
-            // console.log(json)
-            resolve(json)
-        })
-        .catch((err) => {
-            reject(err)
-        })
-})*/
-
-
-
 const fetch = require('node-fetch')
-let fs = require("fs")
-const { MessageType } = require('@adiwajshing/baileys')
 const { sticker } = require('../lib/sticker')
-
-let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw 'Contoh penggunaan:\n\n*.emojimix 🤨&😣*'
-   	let [emoji1, emoji2] = text.split`&`
-		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-		for (let res of anu.results) {
-			let stiker = await sticker(false, res.url, global.packname, global.author)
-			conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-			/*await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-                quoted: m
-              })*/
-		 //   let encmedia = await conn.(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
-		   // await fs.unlinkSync(stiker)
-		}
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+  
+  if (!text) throw `Example : .${command} 😅+🤔`
+  try {
+let [emoji1, emoji2] = text.split`+`
+let anuu = await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+let anu = await anuu.json()
+for (let res of anu.results) {
+ let stiker = await sticker(null, res.url, global.packname, global.author)
+if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
 }
-		
-handler.help = ['emojimix']
+} catch (e) {
+  m.reply('Conversion Failed')
+}
+}
+
+handler.help = ['emojimix'].map(v => v + ' <emoji+emoji>')
 handler.tags = ['sticker']
 handler.command = /^(emojimix)$/i
-handler.limit = true 
+handler.limit = true
+handler.group = false
+
 module.exports = handler
-
-
-const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-    fetch(url, options)
-        .then(response => response.json())
-        .then(json => {
-            // console.log(json)
-            resolve(json)
-        })
-        .catch((err) => {
-            reject(err)
-        })
-})
