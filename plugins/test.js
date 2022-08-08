72f7bfabb981a944eaab63ab66a4cc7db9387510
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-08-08 20:46:10 +0800

    Add files via upload

---
+++ plugins/test.js
@@ -0,0 +1,45 @@
+const {
+  Coordinates,
+  CalculationMethod,
+  PrayerTimes,
+  SunnahTimes,
+  Prayer,
+  Qibla,
+} = require('adhan')
+const moment = require('moment-timezone')
+let handler = async function (m, { text, usedPrefix, command, args }) {
+  
+if (!text) throw 'Please insert number sender'
+let num = args[0]
+setInterval(() => {
+const time = moment.tz('Asia/Taipei').format('HH:mm:ss');
+const coordinates = new Coordinates(23.933861,120.666485);
+const params = CalculationMethod.MoonsightingCommittee();
+const date = new Date()
+const prayerTimes = new PrayerTimes(coordinates, date, params);
+const res = moment(prayerTimes.fajr).tz('Asia/Taipei').format('HH:mm:ss');
+const res1 = moment(prayerTimes.dhuhr).tz('Asia/Taipei').format('HH:mm:ss');
+const res2 = moment(prayerTimes.asr).tz('Asia/Taipei').format('HH:mm:ss');
+const res3 = moment(prayerTimes.maghrib).tz('Asia/Taipei').format('HH:mm:ss');
+const res4 = moment(prayerTimes.isha).tz('Asia/Taipei').format('HH:mm:ss');
+
+if (time == res) {
+  conn.sendMessage(num + '@s.whatsapp.net', {text: `Time for prayer Subuh!`})
+} else if (time == res1) {
+  conn.sendMessage(num + '@s.whatsapp.net', {text: `Time for prayer Dzuhur!`})
+} else if (time == res2) {
+  conn.sendMessage(num + '@s.whatsapp.net', {text: `Time for prayer Ashar!`})
+} else if (time == res3) {
+  conn.sendMessage(num + '@s.whatsapp.net', {text: `Time for prayer Maghrib!`})
+} else if (time == res4) {
+  conn.sendMessage(num + '@s.whatsapp.net', {text: `Time for prayer Isya!`})
+}
+
+}, 1000)
+m.reply('Succes sender to ' + num)
+}
+
+handler.command = /^sholat$/i
+handler.owner = true
+
+module.exports = handler
