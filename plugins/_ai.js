21b984892b1703bcc9276a5e995e32dadd321694
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-09-04 13:37:16 +0800

    Add files via upload

---
+++ plugins/_ai.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+const translate = require('translate-google-api')
+
+let handler = m => m
+handler.before = async function (m, { conn, text }) {
+ let q = m.quoted ? m.quoted : m
+ let mime = (q.msg || q).mimetype
+ if (/webp|image|video|audio/g.test(mime)) return
+if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys)  return
+let anu = await fetch(`http://api.brainshop.ai/get?bid=166135&key=HutlT9ZgXWrPpz3o&uid=uid&msg=${text}`)
+let last = await anu.json()
+let res = await translate(last.cnt, {
+            tld: 'cn',
+            to: 'id',
+        })
+ m.reply(res[0])
+}
+module.exports = handler
