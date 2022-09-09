8635a9e42b063680a9e1c1ef3a26af230ee5870b
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-09-10 06:51:48 +0800

    Add files via upload

--- plugins/_ai.js
+++ plugins/_ai.js
@@ -5,8 +5,12 @@ let handler = m => m
 handler.before = async function (m, { conn, text }) {
  let q = m.quoted ? m.quoted : m
  let mime = (q.msg || q).mimetype
- if (/webp|image|video|audio/g.test(mime)) return
+ if (/webp|image|video/g.test(mime)) return
 if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys)  return
+let trns = await translate(text, {
+            tld: 'cn',
+            to: 'en',
+        })
 let anu = await fetch(`http://api.brainshop.ai/get?bid=166135&key=HutlT9ZgXWrPpz3o&uid=uid&msg=${text}`)
 let last = await anu.json()
 let res = await translate(last.cnt, {
