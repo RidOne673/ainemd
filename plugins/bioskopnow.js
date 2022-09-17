bb71790ff973cb2533d8fb903287eb9ec0d5faa3
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-09-18 06:41:15 +0800

    Add files via upload

---
+++ plugins/bioskopnow.js
@@ -0,0 +1,28 @@
+//let { bioskopNow } = require('@bochilteam/scraper')
+
+let handler = async (m, { conn }) => {
+  let anu = await bioskopNow()
+  anu.forEach(async(data, i) => {
+    setTimeout(() => {
+      txt = `• *Title* : ${data.title}
+• *Url* : ${await shortlink(data.url)}
+• *Genre* : ${data.genre}
+• *Duration* : ${data.duration}
+• *Playing At* : ${data.playingAt}`
+conn.sendMessage(m.chat, {image: {url: data.img}, caption: txt}, {quoted: m})
+    }, 1000* i)
+  })
+  
+}
+
+handler.help = ['bisokopnow']
+handler.tags = ['internet']
+handler.command = /^bioskop|bioskopnow$/i
+handler.limit = false
+
+module.exports = handler
+
+async function shortlink(url) {
+isurl = /https?:\/\//.test(url)
+return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
+}
